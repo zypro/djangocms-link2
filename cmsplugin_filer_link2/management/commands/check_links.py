7f00c32947846e079e1c9911dd9b941922ccfb06
Robert Stein <robert@blueshoe.de>
2018-04-16 14:52:02 +0200

    - ignores ID anchor for checks

--- cmsplugin_filer_link2/management/commands/check_links.py
+++ cmsplugin_filer_link2/management/commands/check_links.py
@@ -13,6 +13,8 @@ class Command(BaseCommand):
     help = 'Check all links for the availability of their destination'
 
     def check_with_request(self, url):
+        if url.startswith('#'):
+            return
         try:
             r = requests.get(url)
         except ConnectionError:
